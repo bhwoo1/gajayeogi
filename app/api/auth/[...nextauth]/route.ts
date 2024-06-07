a7e09dd8a39f96b454af88249ff5e3dee3b71553
Byeonghyeon <problemp123@gmail.com>
2024-06-07 16:27:22 +0900

    네이버 로그인

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth/next";
+import NaverProvider from "next-auth/providers/naver";
+
+const handler = NextAuth({
+  providers: [
+    NaverProvider({
+        clientId: process.env.NAVER_CLIENT_ID || "",
+        clientSecret: process.env.NAVER_CLIENT_SECRET || ""
+    }),
+    
+  ],
+
+
+  pages: {
+    signIn: "/signin",
+  },
+
+});
+
+export { handler as GET, handler as POST };
