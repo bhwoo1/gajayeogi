500d8be7e11f9fc0ed9b229693fad74aab0ec666
Byeonghyeon <problemp123@gmail.com>
2024-06-12 20:01:20 +0900

    오타 수정

--- app/components/layout/MenuBar.tsx
+++ app/components/layout/MenuBar.tsx
@@ -11,7 +11,7 @@ const MenuBar:React.FC = () => {
 
     return(
         <div className="w-full h-full p-4 flex flex-row justify-start">
-            <Link href="/"><div className="pr-24 " onClick={() => setCurrentPage('home')}><p className={currentPage === 'home' ? `font-black text-xl cursor-pointer` : `text-xl font-gray-500`}>홈</p></div></Link>
+            <Link href="/"><div className="pr-24 " onClick={() => setCurrentPage('home')}><p className={currentPage === 'home' ? `font-black text-xl cursor-pointer` : `text-l font-gray-500`}>홈</p></div></Link>
             <Link href="/A"><div className="pr-24 " onClick={() => setCurrentPage('A')}><p className={currentPage === 'A' ? `font-black text-xl cursor-pointer` : `text-l font-gray-400`}>어쩌구</p></div></Link>
             <Link href="/B"><div className="pr-24 " onClick={() => setCurrentPage('B')}><p className={currentPage === 'B' ? `font-black text-xl cursor-pointer` : `text-l font-gray-400`}>저쩌구</p></div></Link>
         </div>
