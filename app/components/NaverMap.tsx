53e4e381a9c49265393c3112e7db306b0ec3f7ae
Byeonghyeon <problemp123@gmail.com>
2024-06-13 15:09:13 +0900

    들여쓰기 수정

--- app/components/NaverMap.tsx
+++ app/components/NaverMap.tsx
@@ -3,38 +3,42 @@
 import { useEffect, useRef } from 'react';
 
 const NaverMap = () => {
-  const mapElement = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
+    const mapElement = useRef<HTMLDivElement>(null);
     const naverMapApiKey = process.env.NAVER_MAP_CLIENT_ID;
 
-    const loadNaverMapScript = () => {
-      const script = document.createElement('script');
-      script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${naverMapApiKey}`;
-      script.async = true;
-      script.onload = () => {
-        initializeMap();
-      };
-      document.head.appendChild(script);
-    };
+    useEffect(() => {
+        
 
-    const initializeMap = () => {
-      if (mapElement.current && window.naver && window.naver.maps) {
-        const map = new window.naver.maps.Map(mapElement.current, {
-          center: new window.naver.maps.LatLng(37.5665, 126.9780), // 예: 서울시청 좌표
-          zoom: 10,
-        });
-      }
-    };
+        const loadNaverMapScript = () => {
+            const script = document.createElement('script');
+            script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=li8s3sk6dv`;
+            script.async = true;
+            script.onload = () => {
+                initializeMap();
+            };
+            document.head.appendChild(script);
+        };
 
-    if (typeof window !== 'undefined' && window.naver && window.naver.maps) {
-      initializeMap();
-    } else {
-      loadNaverMapScript();
-    }
-  }, []);
+        const initializeMap = () => {
+            if (mapElement.current && window.naver && window.naver.maps) {
+                const map = new window.naver.maps.Map(mapElement.current, {
+                    center: new window.naver.maps.LatLng(37.5665, 126.9780), // 예: 서울시청 좌표
+                    zoom: 10,
+                });
+            }
+        };
 
-  return <div ref={mapElement} style={{ width: '100%', height: '400px' }} />;
+        if (typeof window !== 'undefined' && window.naver && window.naver.maps) {
+            initializeMap();
+        }
+        else {
+            loadNaverMapScript();
+        }
+    }, []);
+
+    return (
+        <div ref={mapElement} style={{ width: '100%', height: '400px' }} />
+    );
 };
 
 export default NaverMap;
