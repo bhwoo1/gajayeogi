cee91c71feea03c939a04546bec02303d75f2953
Byeonghyeon <problemp123@gmail.com>
2024-06-13 13:45:57 +0900

    마이페이지 추가, 메뉴바 패딩 변경, 환경변수 추가

--- app/components/layout/MenuBar.tsx
+++ app/components/layout/MenuBar.tsx
@@ -10,24 +10,24 @@ const MenuBar:React.FC = () => {
     const [currentPage, setCurrentPage] = useRecoilState(PageAtom);
 
     return(
-        <div className="w-full h-full p-4 flex flex-row justify-start">
+        <div className="w-full h-full flex flex-row justify-start">
              <Link href="/">
-                <div className="pr-24 cursor-pointer" onClick={() => setCurrentPage('home')}>
-                    <p className={currentPage === 'home' ? 'font-bold text-xl text-gray-800' : 'text-base text-gray-500 hover:text-gray-700'}>
+                <div className="p-8 cursor-pointer " onClick={() => setCurrentPage('home')}>
+                    <p className={currentPage === 'home' ? 'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' : 'text-base text-gray-500 hover:text-gray-700'}>
                         홈
                     </p>
                 </div>
             </Link>
             <Link href="/A">
-                <div className="pr-24 cursor-pointer" onClick={() => setCurrentPage('A')}>
-                    <p className={currentPage === 'A' ? 'font-bold text-xl text-gray-800' : 'text-base text-gray-500 hover:text-gray-700'}>
+                <div className="p-8 cursor-pointer" onClick={() => setCurrentPage('A')}>
+                    <p className={currentPage === 'A' ? 'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' : 'text-base text-gray-500 hover:text-gray-700'}>
                         어쩌구
                     </p>
                 </div>
             </Link>
             <Link href="/B">
-                <div className="pr-24 cursor-pointer" onClick={() => setCurrentPage('B')}>
-                    <p className={currentPage === 'B' ? 'font-bold text-xl text-gray-800' : 'text-base text-gray-500 hover:text-gray-700'}>
+                <div className="p-8 cursor-pointer" onClick={() => setCurrentPage('B')}>
+                    <p className={currentPage === 'B' ? 'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' : 'text-base text-gray-500 hover:text-gray-700'}>
                         저쩌구
                     </p>
                 </div>
---
+++ app/mypage/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from "react"
+
+const MyPage:React.FC = () => {
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>마이 페이지</h1>
+        </main>
+    );
+}
+
+export default MyPage;
