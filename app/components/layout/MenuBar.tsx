decb8ffe4d0af9865b45113da04b1193bd8b4528
Byeonghyeon <problemp123@gmail.com>
2024-06-13 16:30:18 +0900

    코드 정리

--- app/components/layout/MenuBar.tsx
+++ app/components/layout/MenuBar.tsx
@@ -12,22 +12,34 @@ const MenuBar:React.FC = () => {
     return(
         <div className="w-full h-full flex flex-row justify-start">
              <Link href="/">
-                <div className="p-8 cursor-pointer " onClick={() => setCurrentPage('home')}>
-                    <p className={currentPage === 'home' ? 'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' : 'text-base text-gray-500 hover:text-gray-700'}>
+                <div className="p-8 cursor-pointer" onClick={() => setCurrentPage('home')}>
+                    <p className={currentPage === 'home' ? 
+                            'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' 
+                        : 
+                            'text-base text-gray-500 hover:text-gray-700'
+                    }>
                         홈
                     </p>
                 </div>
             </Link>
             <Link href="/A">
                 <div className="p-8 cursor-pointer" onClick={() => setCurrentPage('A')}>
-                    <p className={currentPage === 'A' ? 'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' : 'text-base text-gray-500 hover:text-gray-700'}>
+                    <p className={currentPage === 'A' ? 
+                            'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' 
+                        : 
+                            'text-base text-gray-500 hover:text-gray-700'
+                        }>
                         어쩌구
                     </p>
                 </div>
             </Link>
             <Link href="/B">
                 <div className="p-8 cursor-pointer" onClick={() => setCurrentPage('B')}>
-                    <p className={currentPage === 'B' ? 'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' : 'text-base text-gray-500 hover:text-gray-700'}>
+                    <p className={currentPage === 'B' ? 
+                            'font-bold text-xl text-gray-800 pb-2 border-b-4 border-black' 
+                        : 
+                            'text-base text-gray-500 hover:text-gray-700'
+                        }>
                         저쩌구
                     </p>
                 </div>
