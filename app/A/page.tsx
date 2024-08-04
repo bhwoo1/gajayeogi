f250d8d214f135124fd0cc636ed0d58aabc107b3
Byeonghyeon <problemp123@gmail.com>
2024-08-04 10:07:16 +0900

    투어API 연동 시도

--- app/A/page.tsx
+++ app/A/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import MenuBar from "../components/layout/MenuBar";
 import NaverMap from "../components/NaverMap";
 import Search from "../components/Search";
+import AttractionList from "../components/AttractionList";
 
 const A:React.FC = () => {
     return(
@@ -11,6 +12,7 @@ const A:React.FC = () => {
             <MenuBar currentPage={"A"} />
             <Search />
             <NaverMap />
+            <AttractionList />
         </main>
     );
 }
--- app/components/AttractionList.tsx
+++ app/components/AttractionList.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client"
+
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const AttractionList = () => {
+    useEffect(() => {
+        axios.get("http://localhost:8080/info")
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
+    }, []);
+
+    return(
+        <>
+        
+        </>
+    );
 }
 
 export default AttractionList;
