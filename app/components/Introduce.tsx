2a39f92d2c8027a0a676d8a8cabec9b35f26b675
Byeonghyeon <problemp123@gmail.com>
2024-08-13 18:31:51 +0900

    introduce

--- app/components/Introduce.tsx
+++ app/components/Introduce.tsx
@@ -6,7 +6,7 @@ const Introduce = () => {
     return(
         <main className="flex flex-col items-center p-24">
             <img src="/gajayeogi.png" alt="logo" width='400' height='200' className="mb-2"/>
-            <p>설명 텍스트</p>
+            <p className="text-xl text-gray-800 font-semibold text-center">당신만이 알고 있는 비밀장소, 이제 다른 사람들과 공유해보세요!</p>
         </main>
     );
 
