4faf45d4b2f83d3ac0cb08dc8a6fdda6907c9b50
Byeonghyeon <problemp123@gmail.com>
2024-10-29 16:54:42 +0900

    test

--- app/components/Attraction/UserAttractionList.tsx
+++ app/components/Attraction/UserAttractionList.tsx
@@ -33,7 +33,7 @@ const UserAttractionList = () => {
                 setBestAttractionList(bestAttractions);
             })
             .catch((err) => {
-                console.log(err);
+                console.log('이거 뭐임' + err);
             });
     }, []);
 
