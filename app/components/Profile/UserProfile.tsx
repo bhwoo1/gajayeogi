99d2ae32a97f392cd9c276baa6824e0fa43446b6
Byeonghyeon <problemp123@gmail.com>
2024-10-31 13:56:51 +0900

    유저 프로필 사진 제거

--- app/components/Profile/UserProfile.tsx
+++ app/components/Profile/UserProfile.tsx
@@ -12,11 +12,11 @@ const UserProfile = (props: {username: string, userData: UserData}) => {
     return(
         <div className="flex flex-col">
               <div className="flex flex-row py-12">
-                <div className="w-150 h-150 rounded-full overflow-hidden border-4 border-gray-300">
+                {/* <div className="w-150 h-150 rounded-full overflow-hidden border-4 border-gray-300">
                   {session?.user?.image &&
                     <Image src={`${session?.user.image}`} alt="main" width={150} height={150} />
                   }
-                </div>
+                </div> */}
                 <div className="flex flex-col pl-6 pt-10">
                     <p className="text-3xl font-bold">{props.username} 님.</p>
                     <div className="flex flex-row">
