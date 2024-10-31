912473a55058a670dd0f57d27b39957359b80b92
Byeonghyeon <problemp123@gmail.com>
2024-10-31 13:45:53 +0900

    미리보기 글자수 조정

--- app/components/Attraction/UserAttractionBlock.tsx
+++ app/components/Attraction/UserAttractionBlock.tsx
@@ -7,8 +7,8 @@ const UserAttractionBlock = (props: { attractionData: RecieveAttraction }) => {
     
 
     const makeShortContent = (content: string) => {
-        if (content.length > 10) {
-            return content.substring(0,10) + "...";
+        if (content.length > 20) {
+            return content.substring(0,20) + "...";
         }
         else {
             return content.replace(/§/g, " ");
